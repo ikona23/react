75d111b8525966f5fca531a193b1fa7850df8d70
Peter Kona <ikona23@gmail.com>
2016-09-30 14:56:33 +1300

    reactMagic

--- components/app.js
+++ components/app.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
 
+var divStyle = {
+  color: 'white',
+  fontSize:'1.2em',
+  background: 'rgb(117, 196, 153)',
+  textAlign: 'center',
+  border:'1px solid black'
+};
+
+
 class App extends Component {
 
   constructor (props) {
     super(props)
+    this.state = {
+    }
   }
-
   render () {
-    return <h1>Welcome to {this.props.name}</h1>
+    return (
+      <div style={divStyle}>
+        <h1>hello from navBar</h1>
+      </div>
+    )
   }
-
 }
 
 export default App
